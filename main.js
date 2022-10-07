8e3f31a5b3b32e15f910105d8aa8fad741cc0b6e
DylanMcCarty <dylan.mccarty@student.awesomeinc.org>
2022-10-07 02:06:56 +0000

    removed test code and console logs as well as added winconditions for the state of the board

--- main.js
+++ main.js
@@ -7,14 +7,28 @@ let indexNum = [{
     playerTurn : 0
 }]
 
+function checkWin() {
+    indexNum[0].playerTurn++
+    for (let i of winConditions) {
+        if (gameState[i[0]] === 'X' && gameState[i[1]] === 'X' && gameState[i[2]] === 'X') {
+            clearBoard()
+            winGame()
+        } 
+        if (gameState[i[0]] === 'O' && gameState[i[1]] === 'O' && gameState[i[2]] === 'O') {
+            clearBoard()
+            winGame() 
+        } 
+    }
+    if (indexNum[0].playerTurn === 9) {
+        clearBoard()
+        failGame()
+    }   
+}
 
 function click() {
-    // indexNum[0].playerTurn++
     if (indexNum[0].player === 0) {
         indexNum[0].playerLetter = 'X'
         gameState.splice([indexNum[0].p], 1, indexNum[0].playerLetter)
-        console.log(gameState)
-        // checkWin()
         if (!checkWin()) {
             indexNum[0].player++
         }
@@ -22,7 +36,6 @@ function click() {
         indexNum[0].playerLetter = 'O'
         gameState.splice([indexNum[0].p], 1, indexNum[0].playerLetter)
         console.log(gameState)
-        // checkWin()
         if (!checkWin()) {
             indexNum[0].player-- 
         }
