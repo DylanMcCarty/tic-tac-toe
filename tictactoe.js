8433d9abda266e64ff4c1637c6ee5ff323a09a06
DylanMcCarty <dylan.mccarty@student.awesomeinc.org>
2023-01-30 20:51:12 +0000

    made the game run smoother

--- tictactoe.js
+++ tictactoe.js
@@ -4,3 +4,24 @@
 //     player : null,
 //     turn : 0,
 // }
+
+// function createBoard() {
+//     let mainDiv = document.createElement('div')
+//     let rowDiv = document.createElement('div')
+//     document.body.appendChild(mainDiv)
+//     mainDiv.appendChild(rowDiv)
+//     bootstrap(mainDiv, 'container')
+//     bootstrap(rowDiv, 'row')
+//     for (let i = 0; i < 9; i++) {
+//         let div = document.createElement('div')
+//         div.id = `div${i}`
+//         rowDiv.appendChild(div)
+//         bootstrap(div, 'col border')
+//         div.addEventListener('click')
+//     }
+// }
+// createBoard()
+
+// function bootstrap(element, classes) {
+//     element.className = classes
+// }
